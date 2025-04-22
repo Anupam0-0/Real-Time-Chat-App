125341843395a22d535af203b99796c9504865b0
Anupam0-0 <anupamchoudhary04@gmail.com>
2025-04-23 00:31:43 +0530

    frontend structure built

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
+
 const App = () => {
   return (
-    <div className='bg-neutral-900 text-slate-50/90'>
+    <div data-theme="business" className='min-h-screen '>
         <h1 className='text-4xl'>Hello Larry</h1>
     </div>
   )
